eda4f7cc92de21fc998c5fc9df69af4df480a894
NicolasCallico <115121456+NicolasCallico@users.noreply.github.com>
2026-05-29 17:18:29 -0300

    Update layout.tsx

--- noir-bar/app/layout.tsx
+++ noir-bar/app/layout.tsx
@@ -9,11 +9,15 @@ export const viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
       <body>{children}</body>
     </html>
   );
